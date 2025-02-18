233d0e9c758a5a85ff56333310eec83b4d326c05
DT <kabak133@gmail.com>
2025-02-18 19:23:00 +0300

    custom release

--- release.config.js
+++ release.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-    branches: ["main"], //релизные ветки
+    branches: [ 'main' ], //релизные ветки
+    preset: 'conventionalcommits',
     plugins: [
-        "@semantic-release/commit-analyzer", // Определяет, какой тип коммита сделан (major, minor, patch)
-        "@semantic-release/release-notes-generator", // Генерирует release notes
-        "@semantic-release/changelog", // Обновляет CHANGELOG.md
-        ["@semantic-release/exec", {
-            "prepareCmd": "node -e \"let pkg=require('./package.json'); pkg.version='${nextRelease.version}'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));\""
-        }],
-        ["@semantic-release/git", {
-            "assets": ["package.json", "yarn.lock", "CHANGELOG.md"], // Добавляем package.json и yarn.lock
-            "message": "chore(release): ${nextRelease.version} [skip ci]"
-        }],
+        [ '@semantic-release/commit-analyzer', { // Определяет, какой тип коммита сделан (major, minor, patch)
+            releaseRules: [
+                { type: 'fix', release: 'patch' },
+                { type: 'minor', release: 'minor' },
+                { breaking: true, release: 'major' },
+                { release: 'patch' } // По умолчанию всегда patch
+            ]
+        } ],
+        '@semantic-release/release-notes-generator', // Генерирует release notes
+        '@semantic-release/changelog', // Обновляет CHANGELOG.md
+        [ '@semantic-release/exec', {
+            'prepareCmd': 'node -e "let pkg=require(\'./package.json\'); pkg.version=\'${nextRelease.version}\'; require(\'fs\').writeFileSync(\'package.json\', JSON.stringify(pkg, null, 2));"'
+        } ],
+        [ '@semantic-release/git', {
+            'assets': [ 'package.json', 'yarn.lock', 'CHANGELOG.md' ], // Добавляем package.json и yarn.lock
+            'message': 'chore(release): ${nextRelease.version} [skip ci]'
+        } ],
     ]
-};
+}
