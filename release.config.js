514b1fc3f7597d4249af9eadfa4a11da18c0f32b
DT <kabak133@gmail.com>
2025-02-18 17:21:47 +0300

    fix: npm upload

--- release.config.js
+++ release.config.js
@@ -4,7 +4,6 @@ module.exports = {
         "@semantic-release/commit-analyzer", // Определяет, какой тип коммита сделан (major, minor, patch)
         "@semantic-release/release-notes-generator", // Генерирует release notes
         "@semantic-release/changelog", // Обновляет CHANGELOG.md
-        "@semantic-release/npm", // Обновляет package.json
         ["@semantic-release/git", {
             "assets": ["package.json", "CHANGELOG.md"],
             "message": "chore(release): ${nextRelease.version} [skip ci]"
