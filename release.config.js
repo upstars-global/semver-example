12262dc261f3af249480c16f833c79bd071b6496
DT <kabak133@gmail.com>
2025-02-18 18:22:30 +0300

    fix: try to fix include package.json in release commit

--- release.config.js
+++ release.config.js
@@ -4,12 +4,12 @@ module.exports = {
         "@semantic-release/commit-analyzer", // Определяет, какой тип коммита сделан (major, minor, patch)
         "@semantic-release/release-notes-generator", // Генерирует release notes
         "@semantic-release/changelog", // Обновляет CHANGELOG.md
-        ["@semantic-release/git", {
-            "assets": ["package.json", "yarn.lock", "CHANGELOG.md"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]"
-        }],
         ["@semantic-release/exec", {
             "prepareCmd": "node -e \"let pkg=require('./package.json'); pkg.version='${nextRelease.version}'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));\""
-        }]
+        }],
+        ["@semantic-release/git", {
+            "assets": ["package.json", "yarn.lock", "CHANGELOG.md"], // Добавляем package.json и yarn.lock
+            "message": "chore(release): ${nextRelease.version} [skip ci]"
+        }],
     ]
 };
