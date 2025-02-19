8cde4245ca5c25e202c3e3bede479283820ae403
DT <kabak133@gmail.com>
2025-02-19 11:22:14 +0300

    Refactor release.config.js for consistency and clarity
    Simplified and streamlined syntax by standardizing quotes and spacing. Added a missing release rule for "feat" under minor changes and improved comments for better readability. This ensures a cleaner and more maintainable configuration.

--- release.config.js
+++ release.config.js
@@ -1,26 +1,22 @@
 module.exports = {
-    branches: [ 'main' ], //релизные ветки
-    preset: "conventionalcommits",
+    branches: ['main'], // Релизные ветки
     plugins: [
-        [ '@semantic-release/commit-analyzer', { // Определяет, какой тип коммита сделан (major, minor, patch)
-            "preset": "angular",
+        ['@semantic-release/commit-analyzer', { // Определяет, какой тип коммита сделан (major, minor, patch)
             releaseRules: [
                 { type: "fix", release: "patch" },
                 { type: "perf", release: "patch" },
-                { type: "minor", release: "minor" },
                 { type: "feat", release: "minor" },
+                { type: "minor", release: "minor" },
                 { type: "refactor", release: "minor" },
                 { type: "style", release: "minor" },
                 { type: "docs", release: "minor" },
                 { type: "test", release: "minor" },
                 { type: "chore", release: "minor" },
-                { breaking: true, release: "major" },
-                { type: 'BREAKING_CHANGES', release: 'major' },
+                { breaking: true, release: "major" }, // Ломающее изменение → major
                 { release: "patch" } // По умолчанию всегда patch
-
             ]
-        } ],
-        ["@semantic-release/release-notes-generator", {  // Генерирует release notes
+        }],
+        ['@semantic-release/release-notes-generator', { // Генерирует release notes
             preset: "conventionalcommits",
             presetConfig: {
                 types: [
@@ -37,12 +33,12 @@ module.exports = {
             }
         }],
         '@semantic-release/changelog', // Обновляет CHANGELOG.md
-        [ '@semantic-release/exec', {
-            'prepareCmd': 'node -e "let pkg=require(\'./package.json\'); pkg.version=\'${nextRelease.version}\'; require(\'fs\').writeFileSync(\'package.json\', JSON.stringify(pkg, null, 2));"'
-        } ],
-        [ '@semantic-release/git', {
-            'assets': [ 'package.json', 'yarn.lock', 'CHANGELOG.md' ], // Добавляем package.json и yarn.lock
-            'message': 'chore(release): ${nextRelease.version} [skip ci]'
-        } ],
+        ['@semantic-release/exec', {
+            prepareCmd: 'node -e "let pkg=require(\'./package.json\'); pkg.version=\'${nextRelease.version}\'; require(\'fs\').writeFileSync(\'package.json\', JSON.stringify(pkg, null, 2));"'
+        }],
+        ['@semantic-release/git', {
+            assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]'
+        }]
     ]
-}
+};
