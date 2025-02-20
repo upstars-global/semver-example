6174d06428fddc2ff63da96b8c1a315f0889ff63
DT <kabak133@gmail.com>
2025-02-20 16:09:07 +0300

    perf: test perf tag

--- release.config.js
+++ release.config.js
@@ -58,7 +58,6 @@ module.exports = {
                     } else {
                         newCommit.subject = commitText;
                     }
-
                     return newCommit;
                 }
             },*/
