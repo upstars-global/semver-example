1e21fb987f87b3e9abd3895685544a6c7605ea6d
DT <kabak133@gmail.com>
2025-02-20 12:28:28 +0300

    chore: 2

--- release.config.js
+++ release.config.js
@@ -64,7 +64,7 @@ module.exports = {
         '@semantic-release/changelog',
         ['@semantic-release/exec', {
             prepareCmd: 'node -e "let pkg=require(\'./package.json\'); pkg.version=\'${nextRelease.version}\'; require(\'fs\').writeFileSync(\'package.json\', JSON.stringify(pkg, null, 2));"',
-            successCmd: 'node send-slack-notification.js "${nextRelease.version}" "${process.env.REPO_URL}"'
+            // successCmd: 'node send-slack-notification.js "${nextRelease.version}" "${process.env.REPO_URL}"'
         }],
         ['@semantic-release/git', {
             assets: ['package.json', 'CHANGELOG.md'],
