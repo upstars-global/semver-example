b63800d2be18070711122649b356b432742aeb0e
Ivan Fed <41876762+i-van-develop@users.noreply.github.com>
2025-04-03 14:00:20 +0200

    refactor: semver fixes & docs
    * fix: fix vulnerabilities in release.yaml
    
    * fix: add actual dependencies for project
    
    * feat: update release.config.js commit types behaviour
    
    * docs: add documentation for how to use semantic release commit types in projects
    
    * docs: fix semantic-release.md

--- release.config.js
+++ release.config.js
@@ -9,32 +9,35 @@ function finalizeContext (context) {
         }
     }
 
-    return context
+    return context;
 }
 
 export default {
     branches: ["main", "protected-release-branch"],
     preset: "conventionalcommits",
     plugins: [
-        ["@semantic-release/commit-analyzer", {
+        [ "@semantic-release/commit-analyzer", {
             releaseRules: [
+                { type: "feat", release: "minor" },
+
                 { type: "fix", release: "patch" },
                 { type: "perf", release: "patch" },
-                { type: "feat", release: "minor" },
-                { type: "minor", release: "minor" },
-                { type: "refactor", release: "minor" },
-                { type: "style", release: "minor" },
-                { type: "docs", release: "minor" },
-                { type: "test", release: "minor" },
-                { type: "chore", release: "minor" },
+                { type: "refactor", release: "patch" },
+                { type: "revert", release: "patch" },
+                { type: "chore", release: "patch" },
+
+                { type: "style", release: false },
+                { type: "docs", release: false },
+                { type: "test", release: false },
+
                 { breaking: true, release: "major" },
-                { type: "BREAKING CHANGE", release: "major" },
             ]
-        }],
-        ["@semantic-release/release-notes-generator", {
+        } ],
+
+        [ "@semantic-release/release-notes-generator", {
             preset: "conventionalcommits",
             parserOpts: {
-                noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+                noteKeywords: [ "BREAKING CHANGE", "BREAKING CHANGES" ],
             },
             writerOpts: {
                 commitPartial,
@@ -42,6 +45,7 @@ export default {
             },
             presetConfig: {
                 types: [
+                    { type: "revert", section: "⏪ Reverts", hidden: false },
                     { type: "fix", section: "🐛 Bug Fixes", hidden: false },
                     { type: "feat", section: "🚀 Features", hidden: false },
                     { type: "chore", section: "🔧 Maintenance", hidden: false },
@@ -51,18 +55,18 @@ export default {
                     { type: "perf", section: "⚡ Performance", hidden: false },
                     { type: "test", section: "🧪 Testing", hidden: false },
                     { type: "breaking", section: "⚠ Breaking Changes", hidden: false },
-                    { type: "other", section: "📌 Other Changes", hidden: false }
-                ]
-            }
-        }],
+                    { type: "other", section: "📌 Other Changes", hidden: false },
+                ],
+            },
+        } ],
         "@semantic-release/changelog",
-        ["@semantic-release/exec", {
+        [ "@semantic-release/exec", {
             prepareCmd: "node -e \"let pkg=require('./package.json'); pkg.version='${nextRelease.version}'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));\"",
-            // successCmd: "node send-slack-notification.js \"${nextRelease.version}\" \"${process.env.REPO_URL}\"",
-        }],
-        ["@semantic-release/git", {
-            assets: ["package.json", "CHANGELOG.md"],
-            message: "chore(release): ${nextRelease.version} [skip ci]"
-        }]
-    ]
+            successCmd: "node send-slack-notification.js \"${nextRelease.version}\" \"${process.env.REPO_URL}\"",
+        } ],
+        [ "@semantic-release/git", {
+            assets: [ "package.json", "CHANGELOG.md" ],
+            message: "chore(release): ${nextRelease.version} [skip ci]",
+        } ],
+    ],
 };
